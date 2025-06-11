a6d82343bc3991ae8ec154e870f0e3fc73f9e53b
zjun <565061763@qq.com>
2025-06-11 16:12:33 +0800

    Sample

--- composeApp/src/commonMain/kotlin/com/sd/demo/kmp/paging/RouteScaffold.kt
+++ composeApp/src/commonMain/kotlin/com/sd/demo/kmp/paging/RouteScaffold.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +12,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,10 +29,7 @@ fun RouteScaffold(
         title = { Text(text = title) },
         navigationIcon = {
           IconButton(onClick = onClickBack) {
-            Icon(
-              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-              contentDescription = "Back",
-            )
+            Text(text = "<", fontSize = 24.sp)
           }
         },
       )
